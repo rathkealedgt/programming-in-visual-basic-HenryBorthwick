c8692ba306ce56b3315cac76a521db6ebf70c7b6
HenryBorthwick <51546248+HenryBorthwick@users.noreply.github.com>
2019-08-02 09:48:15 +1200

    Done
    Finished Task Getting Input

--- 02_Get_Input/Module1.vb
+++ 02_Get_Input/Module1.vb
@@ -7,22 +7,35 @@
     ''' 
     Sub Main()
         ' Housekeeping
-
+        Dim UserName, OutputMessage As String
+        Dim FirstNumber, SecondNumber, Sum, Product, SubtractedValue As Integer
 
         ' Ask for user name
-
+        Console.WriteLine("Please enter your name here")
+        UserName = Console.ReadLine()
+        Console.WriteLine(StrReverse(UserName))
 
         ' Ask user for two numbers
+        Console.WriteLine("Please enter your favorite number")
+        FirstNumber = Console.ReadLine()
 
+        Console.WriteLine("Please enter your second favorite number")
+        SecondNumber = Console.ReadLine()
 
         ' Add numbers together
-
+        Sum = FirstNumber + SecondNumber
 
         ' Multiply numbers together
+        Product = FirstNumber * SecondNumber
 
+        'Subtract numbers
+        SubtractedValue = FirstNumber - SecondNumber
 
         ' Greet user and display maths
+        OutputMessage = String.Format("Hello, {0}.  The sum of your number is {1}.  The product of your number is {2}.  And the subtracted value is {3}.", UserName, Sum, Product, SubtractedValue)
+        Console.WriteLine(OutputMessage)
 
+        Console.ReadLine()
 
     End Sub
 
