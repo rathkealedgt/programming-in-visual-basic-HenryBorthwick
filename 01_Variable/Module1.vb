ccfc8ecce0ec72945ae76d84f88c47e5add3607b
HenryBorthwick <51546248+HenryBorthwick@users.noreply.github.com>
2019-08-01 12:32:59 +1200

    Done
    Edited Codw

--- 01_Variable/Module1.vb
+++ 01_Variable/Module1.vb
@@ -3,7 +3,25 @@
     Sub Main()
         Const MYMESSAGE As String = "I heart Computer Science!"
 
+        Dim strMyName As String = 8
+        Const intMyAge As Integer = 15
+        Dim dblDouble As Double = 4.567
+
+
+
         Dim Var14b13 As String = "Weird var name"
+        Dim StudentAge As Integer = 16
+        Dim intStudentAge As Integer = 17
+
+        Console.WriteLine(MYMESSAGE)
+        Console.WriteLine(StudentAge)
+        Console.WriteLine(intStudentAge)
+        Console.WriteLine(Var14b13)
+
+        Console.WriteLine(strMyName)
+        Console.WriteLine(intMyAge)
+
+        Console.ReadLine()
 
     End Sub
 
