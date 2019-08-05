3c3cc207e328f9f04d193860efd7c169c59d97eb
HenryBorthwick <51546248+HenryBorthwick@users.noreply.github.com>
2019-08-06 10:16:07 +1200

    Finished
    Finished Conditional

--- 03_IfAndWhile/Module1.vb
+++ 03_IfAndWhile/Module1.vb
@@ -9,17 +9,28 @@ Module Module1
     Sub Main()
         ' Housekeeping
         Dim UserResponse As String
+        Dim KeepGoing As String = ""
 
-        ' Ask user if they like coffee and record their answer.
-        Console.WriteLine("Do you like coffee?")
-        UserResponse = Console.ReadLine()
+
+        While KeepGoing = ""
+            ' Ask user if they like coffee and record their answer.
+            Console.WriteLine("Do you like coffee?")
+        UserResponse = Console.ReadLine().ToLower()
 
         ' Check input and respond
+        If UserResponse = "yes" Or UserResponse = "y" Then
+            Console.WriteLine("I like coffee too!")
+        ElseIf (UserResponse = "no") Or UserResponse = "n" Then
+                Console.WriteLine("You're not alone")
+            Else
+            Console.WriteLine("I Don't Understand")
+        End If
 
 
-        ' Pause program to let the user read 
-        Console.ReadLine()
-
+            ' Pause program to let the user read 
+            Console.WriteLine("Press q followed by <Enter> to quit")
+            KeepGoing = Console.ReadLine()
+        End While
     End Sub
 
 End Module
