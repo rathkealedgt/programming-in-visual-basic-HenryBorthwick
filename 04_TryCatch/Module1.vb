dd816893d4d95b735150f0290838869ef7afde5a
HenryBorthwick <51546248+HenryBorthwick@users.noreply.github.com>
2019-08-09 15:00:02 +1200

    Finished
    Try Catch Blocks done

--- 04_TryCatch/Module1.vb
+++ 04_TryCatch/Module1.vb
@@ -8,13 +8,32 @@ Module Module1
     Sub Main()
         'Housekeeping
         Dim UserResponse As Integer
+        Const ERROR_MESSAGE As String = "Please enter an integer"
+        Dim ValidInput As Boolean = False
 
-        ' Ask user for a number and store it.
-        Console.WriteLine("Please enter an integer between 1 and 10.")
-        UserResponse = Console.ReadLine()
+        While Not ValidInput
+
+            Try
+                ' Ask user for a number and store it.
+                Console.WriteLine("Please enter an integer between 1 and 10.")
+                UserResponse = Console.ReadLine()
+
+                If 1 <= UserResponse And UserResponse <= 10 Then
+                    ValidInput = True
+                End If
+
+            Catch ex As Exception
+                Console.WriteLine(ERROR_MESSAGE)
+                Console.ReadLine()
+
+            End Try
+        End While
 
         ' Pause the program to view output.
-        Console.ReadLine()
+        Dim Output As String = String.Format("You have entered the number {0}.", UserResponse.ToString)
+            Console.WriteLine(Output)
+            Console.ReadLine()
+
 
     End Sub
 
